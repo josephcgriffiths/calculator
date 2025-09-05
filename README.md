4a4e8b046e3200574c9fe7d7bc44c2b7196b322f
joseph <josephcg.14@gmail.com>
2025-09-05 20:54:47 +0100

    A simple calculator in C

---
+++ README.md
@@ -0,0 +1,6 @@
+# Terminal Calculator
+
+A simple calculator — first program I created in Neovim using C, compiled with clang.
+
+```bash
+clang -Wall -Wextra -g calculator.c -o calc && ./calc
---
+++ calculator.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int add(int a, int b) {
+	return a + b;
+}
+
+int subtract(int a, int b) {
+	return a - b;
+}
+
+int multiply(int a, int b) {
+	return a * b;
+}
+
+int divide(int a, int b) {
+	return a / b;
+}
+
+int main() {
+	int num;
+	int number1;
+	int number2;
+	
+	printf("give one number: ");
+	scanf("%d", &number1);
+	printf("give another number: ");
+	scanf("%d", &number2);
+
+	printf("-----1, for add\n");
+	printf("-----2, for subtract\n");
+	printf("-----3, for multiply\n");
+	printf("-----4 for divide\n");
+
+	printf("Select your function => ");
+	scanf("%d", &num);
+
+	if(num == 1) {
+		int ans = add(number1, number2);
+		printf("Your answer is: %d", ans);
+	}
+	else if (num == 2) {
+		int ans = subtract(number1, number2);
+		printf("Your answer is: %d", ans);
+	}
+	else if (num == 3) {
+		int ans = multiply(number1, number2);
+		printf("Your answer is: %d", ans);
+	}
+	else if (num == 3) {
+		int ans = divide(number1, number2);
+		printf("Your answer is: %d", ans);
+	}
+}	
